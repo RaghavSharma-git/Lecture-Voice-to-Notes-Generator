abff512d11324b0c1a6e7c147a6a05e19b905d3d
RAGHAV SHARMA <gajaniradhasharma@gmail.com>
2026-05-29 17:40:55 +0530

    Update

--- utils/speech_to_text.py
+++ utils/speech_to_text.py
@@ -1,8 +1,7 @@
 import whisper
 
+model = whisper.load_model("base")
+
 def transcribe_audio(file_path):
-    model = whisper.load_model("base")  # good balance speed + accuracy
-    
     result = model.transcribe(file_path)
-    
     return result["text"]
